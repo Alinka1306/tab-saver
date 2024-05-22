05a9f064dc28a5b5fd110f4de1456cacc2890bde
Alina Makarenko <alinamakarenko1306@gmail.com>
2024-05-22 17:57:59 -0500

    70% done - May 22, 2024

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myTabs = [];
+
 const inputEl = document.getElementById("input-el");
 
 const saveInputBtn = document.getElementById("input-btn");
 const saveTabBtn = document.getElementById("input-tab-btn");
 const ulEl = document.getElementById("ul-el");
 
+localStorage.clear();
+let tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"));
+console.log(tabsFromLocalStorage);
+
 saveInputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value);
-    console.log(myLeads);
+    myTabs.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myTabs", JSON.stringify(myTabs));
+    renderTabs();
+    console.log(localStorage.getItem("myTabs"));
 });
 // saveTabBtn.addEventListener("click", function(){
-//     myLeads.push(saveTabBtn);
-//     console.log(myLeads);
+//     myTabs.push(saveTabBtn);
+//     console.log(myTabs);
 // });
 
-for(let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+function renderTabs() {
+    let listItems = "";
+    for(let i = 0; i < myTabs.length; i++) {
+        listItems += `<li><a href='${myTabs[i]}' target='_blank'>${myTabs[i]}</a></li>`;
+    }
+    ulEl.innerHTML = listItems;
 }
