832acc446fbe2a737af24f585dbb2e895b9a0dce
Alina Makarenko <alinamakarenko1306@gmail.com>
2024-05-23 10:38:29 -0500

    finished chrome extension

--- index.js
+++ index.js
@@ -3,29 +3,42 @@ let myTabs = [];
 const inputEl = document.getElementById("input-el");
 
 const saveInputBtn = document.getElementById("input-btn");
-const saveTabBtn = document.getElementById("input-tab-btn");
+const saveTabBtn = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"));
 
-localStorage.clear();
-let tabsFromLocalStorage = JSON.parse(localStorage.getItem("myTabs"));
-console.log(tabsFromLocalStorage);
+if(tabsFromLocalStorage) {
+    myTabs = tabsFromLocalStorage;
+    render(myTabs);
+}
 
 saveInputBtn.addEventListener("click", function(){
     myTabs.push(inputEl.value);
     inputEl.value = "";
     localStorage.setItem("myTabs", JSON.stringify(myTabs));
-    renderTabs();
-    console.log(localStorage.getItem("myTabs"));
+    render(myTabs);
 });
-// saveTabBtn.addEventListener("click", function(){
-//     myTabs.push(saveTabBtn);
-//     console.log(myTabs);
-// });
 
-function renderTabs() {
+saveTabBtn.addEventListener("click", function(){
+    // https://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myTabs.push(tabs[0].url);
+        localStorage.setItem("myTabs", JSON.stringify(myTabs));
+        render(myTabs);
+    });
+});
+
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear();
+    myTabs = [];
+    render(myTabs);
+});
+
+function render(tabs) {
     let listItems = "";
-    for(let i = 0; i < myTabs.length; i++) {
-        listItems += `<li><a href='${myTabs[i]}' target='_blank'>${myTabs[i]}</a></li>`;
+    for(let i = 0; i < tabs.length; i++) {
+        listItems += `<li><a href='${tabs[i]}' target='_blank'>${tabs[i]}</a></li>`;
     }
     ulEl.innerHTML = listItems;
 }
